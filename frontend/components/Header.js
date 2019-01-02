12fd4204aeed13ac4070345a9178921bd46b2a91
Prithwee Das <prithweedas123@gmail.com>
2019-01-02 12:59:59 +0530

    routing in next.js custom App Meta

---
+++ frontend/components/Header.js
@@ -0,0 +1,14 @@
+import Nav from './Nav'
+
+const Header = () => (
+  <div>
+    <div className="bar">
+      <a href="">Sick Fits</a>
+      <Nav />
+    </div>
+    <div className="sub-bar">Search</div>
+    <div>Cart</div>
+  </div>
+)
+
+export default Header

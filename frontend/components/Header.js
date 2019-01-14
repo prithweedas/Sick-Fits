c2a2c82fe7101734cd37f8aea76e7b250a42d3c9
Prithwee Das <prithweedas123@gmail.com>
2019-01-14 20:36:24 +0530

    nprogress

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,7 +1,20 @@
 import Link from 'next/link'
+import Router from 'next/router'
 import styled from 'styled-components'
+import NProgress from 'nprogress'
 
 import Nav from './Nav'
+
+Router.onRouteChangeStart = () => {
+  NProgress.start()
+}
+Router.onRouteChangeComplete = () => {
+  NProgress.done()
+}
+
+Router.onRouteChangeError = () => {
+  NProgress.done()
+}
 
 const Logo = styled.h1`
   font-size: 4rem;
